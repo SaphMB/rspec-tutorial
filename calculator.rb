43d157e69cf4657a2dcb16d01b22b72a698f2dce
Sapphire <saphmb@gmail.com>
2017-03-25 16:40:10 +0000

    first commit

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,5 @@
 class Calculator
+
   def self.add(a, b)
     a + b
   end
@@ -6,4 +7,5 @@ class Calculator
   def self.subtract(a, b)
     a - b
   end
+  
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../calculator'
 
 describe Calculator do
+  
   describe "add" do
     it "returns 3 adding 1 and 2" do
       expect(Calculator.add(1, 2)).to eq(3)
@@ -9,4 +10,11 @@ describe Calculator do
       expect(Calculator.add(1, -2)).to eq(-1)
     end
   end
+  
+  describe "subtract" do
+  	it "return 5 if subtracting 5 from 10" do
+  		expect(Calculator.subtract(10, 5)).to eq(5)
+  		end
+  end
+
 end
